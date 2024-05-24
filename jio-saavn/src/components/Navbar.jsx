bf1b87511503ce0f96b2aa10cc02031622acb6e2
Aman Jindal <151001417+Amanjindal321@users.noreply.github.com>
2024-05-24 19:46:36 +0530

    first commit

---
+++ jio-saavn/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {MdKeyboardArrowDown} from 'react-icons/md'
+const Navbar = () => {
+  return (
+   <nav className='flex justify-between items-center py-3 border-none lg:border px-2 fixed top-0 left-0 right-0 bg-[#f5f5f5ff] z-20'>
+        {/* 1st div */}
+        <div className='flex flex-col lg:flex-row justify-between items-center mx-auto lg:mx-0 '>
+            <div className='flex justify-between items-center gap-2 mr-8'>
+                <img src='/savan-logo.png' alt='logo' width={37} />
+                <Link href="/" className="font-extrabold text-lg">
+                    Jio Saavn
+                </Link>
+            </div>
+            <div className='flex text-[24px] lg:text-[15px] gap-6 text-gray-600 font-semibold h-full'>
+                <li className='list-none'>Music</li>
+                <li className='list-none'>Podcast</li>
+                <li className='list-name'>Go Pro</li>
+            </div>
+        </div>
+
+        {/* 2nd div */}
+        <div className='hidden lg:block'>
+            <input 
+                type="text"
+                name='search'
+                id='search'
+                className='py-2 rounded-full w-[35vw] outline-black text-center border text-black'
+                placeholder='Search for songs...'
+            />
+        </div>
+        {/* 3rd div */}
+        <div className='hidden lg:flex items-center gap-8'>
+            <div className='flex items-center gap-1'>
+                <div className='flex flex-col text-sm'>
+                    <span className='text-[14px] text-gray-500 font-semibold'>Music Language</span>
+                    <span className='text-[14px] text-gray-500 font-semibold'>Hindi</span>
+                </div>
+                <MdKeyboardArrowDown className='text-xl text-gray-500 mt-2' />
+            </div>
+            <div className='flex text-[15px] gap-5 text-gray-600 font-seemibold'>
+                <li className='list-none'>Log In</li>
+                <li className='list-none'>Sign In</li>
+            </div>
+        </div>
+
+   </nav>
+  );
+};
+
+export default Navbar;
---
+++ jio-saavn/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Player from '../components/Player'
+import MainSection from '../components/MainSection';
+
+const Home = () => {
+  return (
+      <>
+        <Navbar />
+        <Player />
+        <MainSection />
+      </>
+  );
+};
+
+export default Home;
